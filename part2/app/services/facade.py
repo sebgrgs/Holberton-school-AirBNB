43066da239a23b970b45d25c7b234875b0ba72ef
sebgrgs <sebastien.georges11@gmail.com>
2025-02-26 16:46:08 +0100

    add amenities API endpoints and facade methods for amenity management

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -36,3 +37,17 @@ class HBnBFacade:
             updated_user = self.get_user(user_id)
 
         return updated_user
+    
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        pass
