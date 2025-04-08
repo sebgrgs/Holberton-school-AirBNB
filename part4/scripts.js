c27e250b78b92ed485fee5ac026272f2a61c9579
sebgrgs <sebastien.georges11@gmail.com>
2025-04-08 16:41:07 +0200

    feat: add favicon and enhance place listing with dynamic content and styling

--- part4/scripts.js
+++ part4/scripts.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+    checkAuthentication();
 });
 
 async function loginUser(email, password) {
@@ -85,4 +86,73 @@ function hideError() {
     if (errorMessage) {
         errorMessage.style.display = 'none';
     }
+}
+
+function checkAuthentication() {
+    const token = getCookie('token');
+    const loginLink = document.getElementById('login-link');
+
+    if (!token) {
+        loginLink.style.display = 'block';
+    } else {
+        loginLink.style.display = 'none';
+        // Fetch places data if the user is authenticated
+    }
+    fetchPlaces(token);
+}
+
+function getCookie(name) {
+    // Function to get a cookie value by its name
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
+async function fetchPlaces(token) {
+    try {
+        const response = await fetch('http://localhost:5000/api/v1/places/', {
+            method: 'GET',
+            headers: {
+                'Authorization': "Token " + token,
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            const data = await response.json();
+            displayPlaces(data);
+        } else {
+            console.error('Failed to fetch places:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching places:', error);
+    }
+}
+
+function displayPlaces(places) {
+    const placesList = document.getElementById('places-list');
+    if (!placesList) return;
+    
+    // Clear the current content
+    placesList.innerHTML = '';
+    
+    // Iterate over the places data
+    places.forEach(place => {
+        const placeElement = document.createElement('div');
+        placeElement.className = 'place-item';
+        
+        placeElement.innerHTML = `
+            <div class="place-card">
+                <h3>${place.title}</h3>
+                <p>$${place.price}</p>
+                <a href="place.html?id=2" class="details-button">View Details</a>
+            </div>
+        `;
+        
+        placesList.appendChild(placeElement);
+    });
 }
